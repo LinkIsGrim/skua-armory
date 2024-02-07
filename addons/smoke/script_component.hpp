b7ca97720acbe1d03021b7797c8131f4dd7c3329
LinkIsGrim <salluci.lovi@gmail.com>
2024-02-07 01:35:51 -0300

    add smoke component

---
+++ addons/smoke/script_component.hpp
@@ -0,0 +1,16 @@
+#define COMPONENT smoke
+#include "\z\skua\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#ifdef DEBUG_ENABLED_SMOKE
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_SMOKE
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_SMOKE
+#endif
+
+#include "\z\skua\addons\main\script_macros.hpp"
