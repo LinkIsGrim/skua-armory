2df6897f9f7461273689aa17795a3b32adfd07be
LinkIsGrim <salluci.lovi@gmail.com>
2024-02-09 01:15:14 -0300

    add CFP subcomponent to weapon_retex

---
+++ addons/weapon_retextures/CFP/CfgWeapons.hpp
@@ -0,0 +1,8 @@
+class CfgWeapons {
+    class LMG_Zafir_F;
+    class CFP_Negev_Black: LMG_Zafir_F {
+        baseWeapon = "LMG_Zafir_blk_F";
+        displayName = CSTRING(LMG_Zafir_blk_Name);
+        picture = QPATHTOF(data\lmg_zafir\icon_LMG_Zafir_black_F_X_ca.paa);
+    };
+};
