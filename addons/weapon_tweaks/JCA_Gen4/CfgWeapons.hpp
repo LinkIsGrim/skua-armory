44940055ab39a22fae426f1f88d3771a07aa7fe0
LinkIsGrim <salluci.lovi@gmail.com>
2024-02-11 02:15:26 -0300

    fix M4A4 AFG magwell

--- addons/weapon_tweaks/JCA_Gen4/CfgWeapons.hpp
+++ addons/weapon_tweaks/JCA_Gen4/CfgWeapons.hpp
@@ -6,6 +6,9 @@ class CfgWeapons {
     class JCA_arifle_M4A4_base_F: Rifle_Base_F {
         magazineWell[] += {"CBA_556x45_STANAG","CBA_556x45_STANAG_L","CBA_556x45_STANAG_XL","CBA_556x45_STANAG_2D","CBA_556x45_STANAG_2D_XL"};
     };
+    class JCA_arifle_M4A4_AFG_base_F: JCA_arifle_M4A4_base_F {
+        magazineWell[] += {"CBA_556x45_STANAG","CBA_556x45_STANAG_L","CBA_556x45_STANAG_XL","CBA_556x45_STANAG_2D","CBA_556x45_STANAG_2D_XL"};
+    };
     class JCA_arifle_SR25_base_F: Rifle_Base_F {
         magazineWell[] += {"CBA_762x51_AR10", "CBA_762x51_AR10_L", "CBA_762x51_AR10_XL"};
     };
