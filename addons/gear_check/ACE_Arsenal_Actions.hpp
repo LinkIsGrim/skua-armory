2bc940aad5b492e93007009128c3f6ade8ba7a1d
LinkIsGrim <salluci.lovi@gmail.com>
2024-02-11 01:21:58 -0300

    gear check condition

--- addons/gear_check/ACE_Arsenal_Actions.hpp
+++ addons/gear_check/ACE_Arsenal_Actions.hpp
@@ -1,7 +1,7 @@
 class ACEGVAR(arsenal,actions) {
     class ADDON {
         displayName = "Gear Checklist";
-        condition = QUOTE([ARR_3(_this select 0,true,true)] call FUNC(showGearCondition));
+        condition = QUOTE([ARR_3(_this select 0,true,false)] call FUNC(showGearCondition));
         updateOnCargoChanged = 1;
         tabs[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14}; // All left panel gear tabs
         class missingBasicGearContent {
